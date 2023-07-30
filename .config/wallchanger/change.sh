1e0d726680e6c7931c70276f3add77e451206aca
Kirill <75751696+nekurillril@users.noreply.github.com>
2023-07-30 18:28:18 +0000

    изменил change.sh

--- .config/wallchanger/change.sh
+++ .config/wallchanger/change.sh
@@ -1,4 +1,9 @@
-find $HOME/Pictures/wallpaper/* | rofi -config .config/wallchanger/config.rasi -dmenu |xargs wal -i 
+#!/bin/bash
+#find $HOME/Pictures/wallpaper/* | rofi -config .config/wallchanger/config.rasi -dmenu |xargs wal -i 
+pic=$HOME/Pictures/wallpaper/
+rofipic="$(ls "$pic" | rofi -dmenu)"
+
+feh --bg-scale "$pic"/"$rofipic"
 wal-telegram --wal --background ~/Downloads/photo_2023-04-12_18-45-08.jpg -g
 killall -q sxhkd
 bspc wm -r
